4e27a0441682fa8971543405fab294ad56baef47
NachoChef <hock3ydud3@gmail.com>
2017-05-05 10:32:04 -0500

    cleanup

--- test.adb
+++
@@ -1,9 +0,0 @@
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with newWay; use newWay;
-
-procedure test is
-   subtype String10 is String(1..10);
-   t : String10 := "0000000002";
-begin
-   makeTree;
-end test;
